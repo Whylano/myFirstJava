ab0203b866ecd99f51bd34e5725215a057bab76f
jkw <jkw95831@naver.com>
2022-09-27 01:49:03 +0900

    2016년

--- helloworl/Years2016.java
+++ helloworl/Years2016.java
@@ -21,3 +21,22 @@ public class Years2016 {
         }
     }
 }
+/* best solution
+import java.util.*;
+
+class TryHelloWorld
+{
+    public String getDayName(int month, int day)
+    {
+        Calendar cal = new Calendar.Builder().setCalendarType("iso8601")
+                        .setDate(2016, month - 1, day).build();
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, new Locale("ko-KR")).toUpperCase();
+    }
+    public static void main(String[] args)
+    {
+        TryHelloWorld test = new TryHelloWorld();
+        int a=5, b=24;
+        System.out.println(test.getDayName(a,b));
+    }
+}
+ */
