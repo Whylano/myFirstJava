d1a5ce553cdf04463ebc0e1c88bf12b09a33edda
jkw <jkw95831@naver.com>
2022-09-26 17:56:49 +0900

    for each구문

--- helloworl/forEachExam.java
+++ helloworl/forEachExam.java
@@ -3,15 +3,17 @@ public class forEachExam {
 
         int[] iarr ={10,20,30,40,50};
 
-        for(int i =0;i< iarr.length; i++){
+        for(int i =0;i< iarr.length; i++){  //일반적인 for array 참조문
             int value = iarr[i];
             System.out.println(value);
         }
-        for(int value:iarr){   //for(타입 값을 받아줄 변수명:출력하고 싶은 자료구조)
+        //훨씬 간결한 for each문
+        for(int value:iarr){   //for each문(타입 값을 받아줄 변수명:출력하고 싶은 자료구조)
             System.out.println(value);
         }
     }
 }
+//for each ,for문
 //  int[] iarr = {10,20,30,40,50};
 //
 //    for(int value:iarr){
