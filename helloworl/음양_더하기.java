b77ea0496d7de57c6489d7ba9d78f043df090392
jkw <jkw95831@naver.com>
2022-09-26 20:05:38 +0900

    음양 더하기

--- helloworl/음양_더하기.java
+++ helloworl/음양_더하기.java
@@ -19,3 +19,12 @@ public class 음양_더하기 {
         }
     }
 }
+//// 다른 풀이
+//class Solution {
+//    public int solution(int[] absolutes, boolean[] signs) {
+//        int answer = 0;
+//        for (int i=0; i<signs.length; i++)
+//            answer += absolutes[i] * (signs[i]? 1: -1);
+//        return answer;
+//    }
+//}
