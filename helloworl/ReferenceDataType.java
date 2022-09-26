f7e0a8a20bc103c47f9c1daf3492705bbe138725
jkw <jkw95831@naver.com>
2022-09-26 18:17:11 +0900

    str기본형을 제외한 것은 모두 참조형 타입이다

---
+++ helloworl/ReferenceDataType.java
@@ -0,0 +1,11 @@
+public class ReferenceDataType {
+    public static void  main(String[] args){
+        int i=4;            //기본형 타입에 4를 저장한다
+        String str = new String("hello");       //str은 참조형 타입
+                                                        //new라는 키워드를 메모리에 올려주세요
+                                                        //메모리에 올라가있는 변수를 인스턴스라 한다.
+                                                        //이런 인스턴스를 str이라고 한다. 말 그대로 참조한다는 뜻이다.
+                                                        //str은 메모리의 위치값을 저장한다.
+                                                        //str변수는 String이라는 인스턴스를 참조한다고 생각하면된다.
+    }
+}
