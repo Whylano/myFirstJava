82ab22c077ab29a2372a8b73f88ef2e9b78412cb
jkw <jkw95831@naver.com>
2022-09-27 11:36:54 +0900

    수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박

--- helloworl/박수박수박수박수박수박수박수박수박수박수.java
+++ helloworl/박수박수박수박수박수박수박수박수박수박수.java
@@ -17,3 +17,18 @@ public class 박수박수박수박수박수박수박수박수박수박수 {
         }
     }
 }
+
+///Best solution
+//public class WaterMelon {
+//    public String watermelon(int n){
+//
+//        return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
+//    }
+//
+//    // 실행을 위한 테스트코드입니다.
+//    public static void  main(String[] args){
+//        WaterMelon wm = new WaterMelon();
+//        System.out.println("n이 3인 경우: " + wm.watermelon(3));
+//        System.out.println("n이 4인 경우: " + wm.watermelon(4));
+//    }
+//}
