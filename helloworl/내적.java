80912302774b30e863ebbf06786890c1121a8c5d
jkw <jkw95831@naver.com>
2022-09-27 09:20:49 +0900

    내적

---
+++ helloworl/내적.java
@@ -0,0 +1,26 @@
+public class 내적 {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int[] a, int[] b) {
+/*문제                int answer = 123456789;
+                return answer*/
+//정답 풀이 //a,b배열의 동일한 인덱스의 요소를 더하고 그 값들을 더해준다.
+                int answer = 0;
+
+                for (int i = 0; i < a.length; i++) {
+                    answer += a[i] * b[i];
+                }
+                return answer;
+
+            }
+        }
+    }
+}
+/*
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        return IntStream.range(0, a.length).map(index -> a[index] * b[index]).sum();
+    }
+ */
