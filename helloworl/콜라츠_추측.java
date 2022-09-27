5c68ee10207a2cecc75f2bf3d4476acbd79ba05b
jkw <jkw95831@naver.com>
2022-09-28 01:42:02 +0900

    콜라츠 추측

--- helloworl/콜라츠_추측.java
+++ helloworl/콜라츠_추측.java
@@ -23,3 +23,19 @@ public class 콜라츠_추측 {
         }
     }
 }
+//class Collatz {
+//    public int collatz(int num) {
+//    long n = (long)num;
+//    for(int i =0; i<500; i++){
+//      if(n==1) return i;
+//      n = (n%2==0) ? n/2 : n*3+1;
+//    }
+//    return -1;
+//  }
+//    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+//    public static void main(String[] args) {
+//        Collatz c = new Collatz();
+//        int ex = 6;
+//        System.out.println(c.collatz(ex));
+//    }
+//}
