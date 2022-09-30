e650fafa751cf3ed399172a481d87f6e9308aa92
jkw <jkw95831@naver.com>
2022-09-30 20:52:00 +0900

    자바 입문주차

---
+++ MyfirstSpring/src/Bus.java
@@ -0,0 +1,14 @@
+public class Bus {
+
+    int member;
+    int nowMemger;
+    int fee;
+    int busnumber;
+    int oil;
+    int nowSpeed;
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
